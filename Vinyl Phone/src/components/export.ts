a58c762f9465bc0d8eb66d294672995aaf911a66
sophienunez <98788221+sophienunez@users.noreply.github.com>
2023-03-31 16:36:20 -0500

    Delete Vinyl Phone directory

--- Vinyl Phone/src/components/export.ts
+++
@@ -1,4 +0,0 @@
-export {default as Player} from "./player/Player";
-export {default as Suggestions} from "./suggestions/Suggestions";
-export {default as Profile} from "./profile/Profile";
-export {default as Navbar} from "./navbar/Navbar";
